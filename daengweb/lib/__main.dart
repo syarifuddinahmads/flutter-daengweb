59308fa7773867ecaa2df07e712e859765b38c63
Ahmad Syarifuddin <udin@gaeasystem.com>
2020-08-22 02:39:12 +0700

    flutter daengweb

---
+++ daengweb/lib/__main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red[800],
+          leading: Icon(Icons.home),
+          title: Text('DW Flutter Basic'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(Icons.archive),
+                  Text('Artikel Terbaru',
+                      style: new TextStyle(fontWeight: FontWeight.bold))
+                ],
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.network(
+                        'https://flutter.io/images/homepage/header-illustration.png'),
+                    Text('Belajar Flutter')
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ daengweb/lib/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Icon(Icons.archive),
+              Text('Artikel Tebaru',style: new TextStyle(fontWeight: FontWeight.bold),)
+            ],
+          ),
+          Card(
+            child: Column(
+              children: <Widget>[
+                Image.network('https://flutter.io/images/homepage/header-illustration.png'),
+                Text('Belajar Flutter')
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
